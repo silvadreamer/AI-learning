7dc023b89e8a558834e7f46d553bc2afc03daec5
SilvaHe <1957915633@qq.com>
2023-04-15 21:30:50 +0800

    knn入门

---
+++ useful functions.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+# 初始化矩阵,全为0
+matrix = np.zeros((2, 3))
+# 获得矩阵的行数
+print(matrix.shape[0])
+# 矩阵整体的复制
+a = np.array([0, 1, 2])
+
+np.tile(a, 2)  # array([0, 1, 2, 0, 1, 2]
+
+np.tile(a, (2, 2))  # array([[0, 1, 2, 0, 1, 2],
+                        #    [0, 1, 2, 0, 1, 2]])
+
+np.tile(a, (2, 1, 2))  # array([[[0, 1, 2, 0, 1, 2]],
+                        #       [[0, 1, 2, 0, 1, 2]]])
+test = np.zeros((1, 20))
+for i in range(20):
+    test[0, i] = int(1)
+    print(test)
